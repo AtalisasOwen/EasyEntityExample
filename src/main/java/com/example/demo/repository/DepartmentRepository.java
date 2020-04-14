50041bb6ae970afebd6d8dc360e840554fec0718
10221903 <742137319@qq.com>
2020-04-14 09:08:51 +0800

    remove generated code

--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++
@@ -1,3 +0,0 @@
-package com.example.demo.repository;
-
-public interface DepartmentRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.demo.entity.Department, java.lang.Long>, org.springframework.data.jpa.repository.JpaSpecificationExecutor<com.example.demo.entity.Department> {}
--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++
@@ -1,3 +0,0 @@
-package com.example.demo.repository;
-
-public interface EmployeeRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.demo.entity.Employee, java.lang.Long>, org.springframework.data.jpa.repository.JpaSpecificationExecutor<com.example.demo.entity.Employee> {}
